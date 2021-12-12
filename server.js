1a47fb10f435f2d5223e20b351a3ae2c82fa4354
Djamel Ramdani <enzonpapa@gmail.com>
2021-12-11 22:15:19 -0600

    server

--- server.js
+++ server.js
@@ -2,12 +2,25 @@ const express = require("express");
 const axios = require("axios");
 const db = require("./db/connection");
 
-
 const PORT = process.env.PORT || 3001;
 const app = express();
-
-
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Default response for request (Not Found)
+app.use((req, res) => {
+        res.status(404).end();
+      });
+      
+      // Start server when DB connected 
+      db.connect((err) => {
+        if (err) return console.log(err);
+        console.log("Database connected.");
+        
+      });
+      
+      app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+      });
